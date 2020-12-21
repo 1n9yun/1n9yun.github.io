29115df9466e1325550dd3a299c423eb277c2962
1n9yun <jig7357@gmail.com>
2020-12-21 19:20:18 +0900

    add sensor-monitor project

--- assets/js/swiper-control.js
+++ assets/js/swiper-control.js
@@ -22,7 +22,7 @@ function swiperInitialize(container_id, props){
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-          delay: 2500,
+          delay: props.millis,
           disableOnInteraction: false,
         },
         pagination: {
