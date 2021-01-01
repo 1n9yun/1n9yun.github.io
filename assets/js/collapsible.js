2485cc6edf4a115b835e198e2da9416d1ac4360d
1n9yun <jig7357@gmail.com>
2021-01-02 00:13:16 +0900

    test

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -10,7 +10,7 @@ function collapsibleInit(){
     titleElement = document.getElementsByClassName("collapsible-title")[0];
     bodyElement = document.getElementsByClassName("collapsible-body")[0];
     folded = bodyElement.hasAttribute("open") ? false : true;
-
+    console.log(folded)
     headerElement = titleElement.children[0];
     headerElement.innerHTML = 
     `<span class="collapsible-mark">` + (folded ? `✊` : `🖐`) + `</span>` + headerElement.innerHTML;
