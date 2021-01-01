4bf24b92ac42b3b48a727f835881912fa3d67730
1n9yun <jig7357@gmail.com>
2021-01-02 00:09:07 +0900

    add problem-solving post boj1185

---
+++ _featured_categories/algorithm/theory.md
@@ -0,0 +1,13 @@
+---
+layout: post
+category: algorithm
+title: Theory
+description: >
+  알고리즘 이론 모음
+related_posts:
+  - 
+addons:
+  - related
+  - comments
+list: true
+---
---
+++ assets/js/collapsible.js
@@ -0,0 +1,27 @@
+// use one component only per page
+
+let folded;
+let titleElement;
+let headerElement;
+let markElement;
+let bodyElement;
+
+function collapsibleInit(){
+    titleElement = document.getElementsByClassName("collapsible-title")[0];
+    bodyElement = document.getElementsByClassName("collapsible-body")[0];
+    folded = bodyElement.hasAttribute("open") ? false : true;
+
+    headerElement = titleElement.children[0];
+    headerElement.innerHTML = 
+    `<span class="collapsible-mark">` + (folded ? `✊` : `🖐`) + `</span>` + headerElement.innerHTML;
+
+    markElement = document.getElementsByClassName("collapsible-mark")[0];
+
+    titleElement.addEventListener("click", collapsibleClick);
+}
+
+function collapsibleClick(){
+    folded = !folded;
+    markElement.innerHTML = folded ? '✊' : '🖐';
+    bodyElement.style.display = folded ? 'none' : 'block';
+}
